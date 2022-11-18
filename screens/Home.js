ed0c1a37ec1851441a115ef5c140fb31096d0d31
jothamardel <ardelmbiplang@gmail.com>
2022-11-18 15:30:45 +0100

    chore: implementing search functionality in Home screen

--- screens/Home.js
+++ screens/Home.js
@@ -1,21 +1,36 @@
 import { useState } from "react";
-import { Text, View, SafeAreaView, FlatList } from "react-native";
+import { View, SafeAreaView, FlatList } from "react-native";
 
 import { COLORS, NFTData } from "../constants";
 import { NFTCard, HomeHeader, FocusedStatusBar } from "../components";
 
 const Home = () => {
+  const [nftData, setnftData] = useState(NFTData);
+
+  const handleSearch = (value) => {
+    if (!value.length) return setnftData(NFTData);
+
+    const filteredData = NFTData.filter(item => 
+      item.name.toLowerCase().includes(value.toLowerCase()));
+
+    if (filteredData.length) {
+      return setnftData(filteredData);
+    }
+
+    setnftData(NFTData);
+
+  }
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <FocusedStatusBar background={COLORS.primary}/>
       <View style={{ flex: 1 }}>
         <View style={{ zIndex: 0 }}>
           <FlatList 
-            data={NFTData}
+            data={nftData}
             renderItem={({ item }) => <NFTCard data={item}/>}
             keyExtractor={(item) => item.id}
             showsVerticalScrollIndicator={false}
-            ListHeaderComponent={<HomeHeader />}
+            ListHeaderComponent={<HomeHeader onSearch={handleSearch}/>}
           />
         </View>
         <View style={{ 
